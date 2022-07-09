007d6b4598178cdde68380d40559cddfda3aecac
Vaidotas Piekus <vaidotas@remote.com>
2022-07-09 15:34:14 +0200

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import Head from "next/head";
-import Image from "next/image";
 
 interface URLInputProps {
 	value: string;
@@ -13,16 +11,18 @@ const URLInput = ({ value = "", setUrlVal }: URLInputProps) => {
 	}
 
 	return (
-		<>
-			<input
-				className="border-dashed border-2 border-fuchsia-300 w-96 p-1"
-				type="text"
-				placeholder="url"
-				value={value}
-				onChange={(e) => handleUrlInputChange(e.target.value)}
-			/>
-		</>
+		<input
+			className="border-dashed border-2 border-fuchsia-300 w-96 p-1"
+			type="text"
+			placeholder="url"
+			value={value}
+			onChange={(e) => handleUrlInputChange(e.target.value)}
+		/>
 	);
+};
+
+const Output = () => {
+	return <div>output</div>;
 };
 
 const Home: NextPage = () => {
@@ -30,11 +30,15 @@ const Home: NextPage = () => {
 	const [validationErr, setValidationErr] = useState("");
 
 	function handleShorten() {
-		if (!!urlVal) {
+		const trimVal = urlVal.trim();
+		console.log(urlVal);
+		if (!trimVal) {
 			console.log("nothing there!");
 			setValidationErr("input url first!");
 			return;
 		}
+
+		setValidationErr("");
 	}
 
 	return (
@@ -46,17 +50,24 @@ const Home: NextPage = () => {
 				<URLInput value={urlVal} setUrlVal={(val: string) => setUrlVal(val)} />
 				<div className="flex flex-row gap-2 mt-2">
 					<button
-						onClick={() => setUrlVal("")}
+						onClick={() => {
+							setUrlVal("");
+							setValidationErr("");
+						}}
 						className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
 					>
 						clear
 					</button>
 					<button
+						type="submit"
 						onClick={handleShorten}
 						className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
 					>
 						shorten
 					</button>
+				</div>
+				<div>
+					{!!validationErr ? <div>Err: {validationErr}</div> : <Output />}
 				</div>
 			</div>
 		</div>
