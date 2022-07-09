534976dc57310b5b9a37fee0c60c29407cf72b38
Vaidotas Piekus <vaidotas@remote.com>
2022-07-09 15:07:08 +0200

    buttons buttons for the win

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,63 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
-const URLInput = () => {
+interface URLInputProps {
+	value: string;
+	setUrlVal: (val: string) => void;
+}
+const URLInput = ({ value = "", setUrlVal }: URLInputProps) => {
+	function handleUrlInputChange(val: string) {
+		setUrlVal(val);
+	}
+
 	return (
 		<>
 			<input
 				className="border-dashed border-2 border-fuchsia-300 w-96 p-1"
 				type="text"
+				placeholder="url"
+				value={value}
+				onChange={(e) => handleUrlInputChange(e.target.value)}
 			/>
-			<div className="flex flex-row gap-2 mt-2">
-				<button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-					clear
-				</button>
-				<button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-					shorten
-				</button>
-			</div>
 		</>
 	);
 };
 
 const Home: NextPage = () => {
+	const [urlVal, setUrlVal] = useState("");
+	const [validationErr, setValidationErr] = useState("");
+
+	function handleShorten() {
+		if (!!urlVal) {
+			console.log("nothing there!");
+			setValidationErr("input url first!");
+			return;
+		}
+	}
+
 	return (
 		<div className="flex flex-col gap-3 justify-start w-screen h-screen">
 			<div className="p-3 items-center flex flex-col">
 				<h2 className="text-3xl text-blue-500 self-center mt-6 mb-6">
 					URL Shortener
 				</h2>
-				<URLInput />
+				<URLInput value={urlVal} setUrlVal={(val: string) => setUrlVal(val)} />
+				<div className="flex flex-row gap-2 mt-2">
+					<button
+						onClick={() => setUrlVal("")}
+						className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+					>
+						clear
+					</button>
+					<button
+						onClick={handleShorten}
+						className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+					>
+						shorten
+					</button>
+				</div>
 			</div>
 		</div>
 	);
